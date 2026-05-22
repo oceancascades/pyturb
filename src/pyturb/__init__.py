fdab4df8b8162792fba421ae0e0aa496ffc301b5
Jesse Cusack <jesse.cusack@oregonstate.edu>
2026-05-22 16:58:00 +0200

    shuffle things so that main processing steps are clearer

--- src/pyturb/__init__.py
+++ src/pyturb/__init__.py
@@ -5,9 +5,10 @@ This package provides tools for reading and processing data from RSI
 microstructure instruments (P-files) and analyzing turbulence quantities.
 """
 
-from . import io, merge, pfile, processing, profile, shear, viscosity
+from . import auxiliary, io, merge, pfile, processing, profile, shear, viscosity
 
 __all__ = [
+    "auxiliary",
     "io",
     "merge",
     "pfile",
